050a97f3390e18596cdf0af8149082e283187e3b
Matthias Nagel <matthinagel@gmail.com>
2021-11-21 12:57:33 +0100

    Removes warnings

--- src/Evaluation.hs
+++ src/Evaluation.hs
@@ -19,9 +19,9 @@ eval = evalWithContext mempty
 evalWithContext :: VariableExpressionContext -> Expression -> Maybe Expression
 evalWithContext context atom@(CAtom _) = return atom
 evalWithContext context fun@(CLambda _ _) = return fun
-evalWithContext context (EVar varname) = case Map.lookup varname context of
-    Nothing -> Nothing
-    Just exp -> evalWithContext context exp
+evalWithContext context (EVar varname) = Map.lookup varname context >>= evalWithContext context 
 evalWithContext context (EApplication fun arg) = case evalWithContext context fun of
     Nothing -> Nothing
     Just (CLambda varname exp) -> evalWithContext (Map.insert varname arg context) exp
+    Just _ -> Nothing
+evalWithContext _ _ = Nothing 
