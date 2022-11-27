cf7fb6211f6c452f7dfcf5c80a13f3eee6c784c1
Matthias Nagel <matthinagel@gmail.com>
2022-11-27 18:21:32 +0100

    Strict types

--- src/Types.hs
+++ src/Types.hs
@@ -7,11 +7,11 @@ module Types
 where
 
 data Expression
-  = CAtom String
-  | CLambda String Expression
-  | EVar String
-  | EApplication Expression Expression
-  | Athe Expression Type
+  = CAtom !String
+  | CLambda !String !Expression
+  | EVar !String
+  | EApplication !Expression !Expression
+  | Athe !Expression !Type
   deriving (Eq, Show)
 
-data Type = Atom | Arrow Type Type deriving (Eq, Show)
+data Type = Atom | Arrow !Type !Type deriving (Eq, Show)
